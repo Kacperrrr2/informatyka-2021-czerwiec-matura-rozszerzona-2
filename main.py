4a640a1acc2fe97cd881b69540e4f16f14ac8e88
Kacper <144324382+Kacperrrr2@users.noreply.github.com>
2024-03-15 08:14:57 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+wiersze=open('przyklad.txt','r')
+tab=[]
+for line in wiersze:
+    tab.append(line.strip())
+
+
+# ZAD4.1
+# liczba_cyfr=0
+# for i in tab:
+#     for j in i:
+#         if j=="1" or j=="2"or j=="3" or j=="4" or j==" " or j=="5" or j=="6"or j=="7" or j==" " or j=="8" or j=="9" or j=="0":
+#             liczba_cyfr=liczba_cyfr+1
+#
+# print(liczba_cyfr)
+
+# ZAD4.2
+# string=""
+# licznik=0
+# for i in range(19,len(tab),20):
+#        string+=tab[i][licznik]
+#        licznik+=1
+#
+# print(string)
+
+
+#ZAD 4.3
+
+
+
+
+#ZAD4.4
+
+
